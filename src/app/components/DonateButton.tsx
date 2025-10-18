f01fab99fec7ce3460dde4a08e5975a15ffdb559
DYAPA GNANESHWAR REDDY <gnaneha.9206@gmail.com>
2025-10-18 18:33:11 +0530

    Update DonateButton.tsx

--- src/app/components/DonateButton.tsx
+++ src/app/components/DonateButton.tsx
@@ -45,7 +45,7 @@ export default function DonateButton() {
               <button
                 onClick={() => {
                   setIsDialogOpen(false);
-                  router.push('mailto:cvrikshana@gmail.com?subject=DonationIntrest);
+                  router.push('mailto:cvrikshana@gmail.com?subject=DonationIntrest');
                 }}
                 className="bg-blue-600 text-white px-6 py-2 rounded-full hover:bg-blue-700 transition-colors"
               >
